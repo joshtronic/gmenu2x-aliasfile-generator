0a2525ec978fb1402f1fd0f7b5b8e96e268580c1
Josh Sherman <joshsherman@gmail.com>
2023-03-20 22:29:32 -0500

    feat: first pass, good enough

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# gmenu2x-aliasfile-generator
-Generates a gmenu2x aliasfile from a directory of files
+# GMenu2X aliasfile generator
+
+Generates a GMenu2X aliasfile from a directory of files
+
+## Usage
+
+Generate to standard out:
+
+```shell
+npm start -- --directory /path/to/roms
+```
+
+Pipe it to an aliasfile:
+
+```shell
+npm start -- -d /path/to/roms > /path/to/aliasfile
+```
---
+++ src/index.mjs
@@ -0,0 +1,35 @@
+import { promises as fs } from 'fs';
+import { program } from 'commander';
+
+program
+  .requiredOption('-d, --directory <path>', 'directory to read')
+  .parse();
+
+const { directory } = program.opts();
+
+const files = await fs.readdir(directory);
+
+files.forEach((file) => {
+  let alias = file.slice(0, file.lastIndexOf('.'));
+
+  // Awesome gist that inspired most of the following:
+  // https://gist.github.com/ramiabraham/ff41ba74f2b7104ecece
+  alias = alias
+    // Removes primary ROM codes
+    .replace(/\[(a|p|b|f|T-|T+|t|h|o|J|!)\]/gi, '')
+    .replace(/\((-|M\d+|U|E|UE)\)/gi, '')
+    // TODO: Stripe checksums - (###)
+    // Removes country codes
+    .replace(/\((1|4|A|J|B|K|C|NL|E|PD|F|S|F|FC|SW|FN|U|G|UK|GR|Unk|HK|I|H|Unl)\)/gi, '')
+    // Removes Nintendo Game Boy specific codes
+    .replace(/\[(C|S|BF)\]/gi, '')
+    // Removes Super Nintendo Entertainment System specific codes
+    .replace(/\((BS|ST|NP)\)/gi, '')
+    // Removes Sega Genesis specific codes
+    .replace(/\((1|4|5|8)\)/gi, '')
+    .replace(/\[ (\(B\) Brazil|\[c\] Checksum|\[x\] Bad Checksum|\[R-\] Countries) \]/gi, '')
+    // Removes Nintendo Entertainment System specific codes
+    .replace(/\[(PC10|VS)\]/gi, '');
+
+  console.log(`${file}=${alias}`);
+});
